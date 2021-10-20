3bb249d95e68e5b1bf262ee39e27f8526684e5e4
Alain Buisseret <me@alain.dev>
2021-10-20 22:51:10 +0200

    Utilisation des séquences VT100
    Pour manipuler les terminaux compatibles VT100, il existe des [séquences
    d'échapement](https://www2.ccs.neu.edu/research/gpc/VonaUtils/vona/terminal/vtansi.htm)
    qui permettent plein de trucs sympas.
    Dans le fichier `util.c`, j'ai modifié la fonction `clearscr` de manière
    à ce qu'elle vide le terminal et qu'elle déplace le curseur en haut à
    droite.

--- util.c
+++ util.c
@@ -1,15 +1,7 @@
 #include "util.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 void clearscr()
 {
-#ifdef _WIN32
-    system("cls");
-#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
-    system("clear");
-//add some other OSes here if needed
-#else
-    #error "OS not supported."
-    //you can also throw an exception indicating the function can't be used
-#endif
+	puts("\x1b[2J\x1b[1;1H");
 }
